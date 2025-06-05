e28c274e71b11ea61d14bcb3d558e445cc7a44d6
fogazza <vfogacacintra@gmail.com>
2025-06-05 11:21:41 -0300

    05-06-25 / feat: Aprimora validação de usuário e tratamento de erros
    - Atualiza validação para permitir um dado ser alterado por vez, no mínimo um dado deve ser alterado.
    - Remove lógica redundante de verificação de dados iguais antes da atualização.
    - Exclui emails educacionais não usados.

--- src/services/validateUsuario.js
+++ src/services/validateUsuario.js
@@ -5,30 +5,33 @@ module.exports = {
   // Valida os campos obrigatórios para criação do usuário
   validateUsuario: function ({ NIF, email, senha, nome, confirmarSenha }) {
     const senaiDomains = [
-      "@edu.senai.br",
       "@docente.senai.br",
-      "@sesisenaisp.org.br"
     ];
-  
+
     if (!NIF || !email || !senha || !nome || !confirmarSenha) {
       return { error: "Todos os campos devem ser preenchidos" };
     }
     if (isNaN(NIF) || NIF.length !== 7) {
-      return { error: "NIF inválido. Deve conter exatamente 7 dígitos numéricos" };
+      return {
+        error: "NIF inválido. Deve conter exatamente 7 dígitos numéricos",
+      };
     }
     if (!email.includes("@")) {
       return { error: "Email inválido. Deve conter @" };
     }
     const emailDomain = email.substring(email.lastIndexOf("@"));
     if (!senaiDomains.includes(emailDomain)) {
-      return { error: "Email inválido. Deve pertencer a um domínio SENAI autorizado" };
+      return {
+        error: "Email inválido. Deve pertencer a um domínio SENAI autorizado",
+      };
     }
-    if (senha != confirmarSenha){
+    if (senha != confirmarSenha) {
       return { error: "As senhas não coincidem" };
     }
     if (!validarSenha(senha)) {
       return {
-        error: "A senha deve ter no mínimo 8 caracteres, incluindo letras, números e um caractere especial.",
+        error:
+          "A senha deve ter no mínimo 8 caracteres, incluindo letras, números e um caractere especial.",
       };
     }
   },
@@ -44,7 +47,9 @@ module.exports = {
           return reject("Erro ao verificar NIF ou email");
         }
         if (results.length > 0) {
-          return resolve({ error: "O NIF ou email já está vinculado a outro usuário" });
+          return resolve({
+            error: "O NIF ou email já está vinculado a outro usuário",
+          });
         }
         return resolve(null);
       });
@@ -54,9 +59,7 @@ module.exports = {
   // Valida os campos para login
   validateLogin: function ({ email, senha }) {
     const senaiDomains = [
-      "@edu.senai.br",
       "@docente.senai.br",
-      "@sesisenaisp.org.br"
     ];
     if (!email || !senha) {
       return { error: "Todos os campos devem ser preenchidos" };
@@ -66,37 +69,46 @@ module.exports = {
     }
     const emailDomain = email.substring(email.lastIndexOf("@"));
     if (!senaiDomains.includes(emailDomain)) {
-      return { error: "Email inválido. Deve pertencer a um domínio SENAI autorizado" };
-    }
-    return null;
-  },
-
-  // Valida os campos para atualização do usuário
-  validateUpdateUsuario: function ({ email, senha, nome}) {
-    const senaiDomains = [
-      "@edu.senai.br",
-      "@docente.senai.br",
-      "@sesisenaisp.org.br"
-    ];
-
-    if (!email || !senha || !nome) {
-      return { error: "Todos os campos devem ser preenchidos" };
-    }
-    if (!email.includes("@")) {
-      return { error: "Email inválido. Deve conter @" };
-    }
-    const emailDomain = email.substring(email.lastIndexOf("@"));
-    if (!senaiDomains.includes(emailDomain)) {
-      return { error: "Email inválido. Deve pertencer a um domínio SENAI autorizado" };
-    }
-    if (!validarSenha(senha)) {
       return {
-        error: "A senha deve ter no mínimo 8 caracteres, incluindo letras, números e um caractere especial.",
+        error: "Email inválido. Deve pertencer a um domínio SENAI autorizado",
       };
     }
     return null;
   },
 
+  validateUpdateUsuario: function ({ email, senha, nome }) {
+    const senaiDomains = [
+      "@docente.senai.br",
+    ];
+
+    if (email === undefined && senha === undefined && nome === undefined) {
+      return { error: "Pelo menos um campo deve ser preenchido" };
+    }
+
+    if (email !== undefined) {
+      if (!email.includes("@")) {
+        return { error: "Email inválido. Deve conter @" };
+      }
+      const emailDomain = email.substring(email.lastIndexOf("@"));
+      if (!senaiDomains.includes(emailDomain)) {
+        return {
+          error: "Email inválido. Deve pertencer a um domínio SENAI autorizado",
+        };
+      }
+    }
+
+    if (senha !== undefined) {
+      if (!validarSenha(senha)) {
+        return {
+          error:
+            "A senha deve ter no mínimo 8 caracteres, incluindo letras, números e um caractere especial.",
+        };
+      }
+    }
+
+    return null;
+  },
+  
   // Valida se o ID do usuário foi fornecido
   validateUsuarioId: function (id_usuario) {
     if (!id_usuario) {
@@ -105,12 +117,11 @@ module.exports = {
     return null;
   },
 
-    // Valida se o ID do usuário foi fornecido
-    validateUsuarioEmail: function (email) {
-      if (!email) {
-        return { error: "Email do usuário é obrigatório" };
-      }
-      return null;
-    },
-
+  // Valida se o ID do usuário foi fornecido
+  validateUsuarioEmail: function (email) {
+    if (!email) {
+      return { error: "Email do usuário é obrigatório" };
+    }
+    return null;
+  },
 };
